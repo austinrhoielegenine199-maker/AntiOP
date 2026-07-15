d3c238a975e4e364a63ab3405b98bee136d84df0
austinrhoielegenine199-maker <austinrhoielegenine199@gmail.com>
2026-07-16 05:37:08 +0800

    Update AntiOP.java

--- src/main/java/me/antiop/AntiOP.java
+++ src/main/java/me/antiop/AntiOP.java
@@ -1,1 +1,25 @@
+package me.antiop;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiOP extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("AntiOP enabled!");
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        String cmd = event.getMessage().toLowerCase();
+
+        if (cmd.equals("/op") || cmd.startsWith("/op ")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("§cYou cannot use /op!");
+        }
+    }
+}
